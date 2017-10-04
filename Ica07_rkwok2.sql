1d0341894e02cbb3ad1f79fd4f15c8c479172a50
Ryan Kwok <rkwok2@nait.ca>
2017-10-04 13:27:39 -0600

    on question q7

--- Ica07_rkwok2.sql
+++ Ica07_rkwok2.sql
@@ -22,7 +22,45 @@ go
 
 --q3--
 select ProductName as ' Product Name', UnitPrice as 'Unit Price' from Products
-where EmployeeID in {select EmployeeID
-					 from  Employees
-					 where Country
-									
+where SupplierID in (select SupplierID
+					 from  Suppliers
+					 where Country in ('Sweden','Italy'))
+order by UnitPrice
+go
+
+--q4--
+select ProductName as 'Product Name', UnitPrice as 'Unit Price' from Products
+where exists (select SupplierID
+			  from Suppliers
+			  where Country in ('Sweden','Italy')And Suppliers.SupplierID = Products.SupplierID) --Making sure you are comapring supplier ids from both tables being the same-- 
+order by UnitPrice
+go
+
+--q5--
+declare @minPrice int = 20
+
+select ProductName as 'Product Name' from Products
+where CategoryID in (select CategoryID
+					 from Categories
+					 where CategoryName in ('Seafood','Confections')) and (UnitPrice > @minPrice)
+order by CategoryID asc, ProductName asc
+go
+
+--q6--
+declare @minPrice int = 20
+select ProductName as 'Product Name'from Products
+where exists (select CategoryName
+			  from Categories
+			  where CategoryName in ('Seafood','Confections') and Categories.CategoryID = products.CategoryID) and (UnitPrice > @minPrice)
+order by  CategoryID asc, ProductName asc
+go
+
+
+
+
+
+
+
+
+						
+										
